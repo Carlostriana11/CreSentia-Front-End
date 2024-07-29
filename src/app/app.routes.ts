86decbbea1b064e515368b588d0be64b57314955
Carlos Triana <carlostriana404@gmail.com>
2024-07-29 12:31:22 -0500

    se creo las rutas e navegacion y la redireccion por si ingresan a una ruta que no existe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+        children: [
+            {
+                path:'home',
+                title: 'Home',
+                loadComponent: () => import('./dashboard/pages/home/home.component'),
+            },
+            {
+                path:'products',
+                title:'Products',
+                loadComponent: () => import('./dashboard/pages/products/products.component'),
+            },
+            {
+                path:'',redirectTo:'home', pathMatch:'full',
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path:'**',
+        redirectTo: 'dashboard',
+        pathMatch:'full'
+    }
+];
