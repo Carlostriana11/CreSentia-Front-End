df730b45b88e31e6dec96aa02935518da062f529
Carlos Triana <carlostriana404@gmail.com>
2024-07-31 13:35:43 -0500

    termino de crear el html y css y la informacion de cada cart y se modifico la barra de navegacion,se le agrego logo

--- src/app/components/public/cart/cart.component.ts
+++ src/app/components/public/cart/cart.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrls: ['./cart.component.css'] // Corrige a styleUrls
 })
 export class CartComponent {
-
+  @Input() logoSrc: string = '';
+  @Input() fotoSrc: string = '';
+  @Input() title: string = '';
+  @Input() parrafoClave: string = '';
+  @Input() parrafoRelleno: string = '';
+  @Input() parrafoFinal: string = '';
+  @Input() containerBackgroundColor: string = '#fff';
+  @Input() url: string = "/";
+  @Input() text: string = "";
 }
--- src/app/dashboard/pages/products/products.component.ts
+++ src/app/dashboard/pages/products/products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CartComponent } from "../../../components/public/cart/cart.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CartComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
